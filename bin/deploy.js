ced59cfed3aca76d27a8a85b900fb5be4f9396bc
Jakub Sokołowski <jakub@status.im>
2019-10-14 12:46:56 +0200

    add a CI deployment setup
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ bin/deploy.js
@@ -0,0 +1,31 @@
+const { promisify } = require('util')
+const { publish } = require('gh-pages')
+const ghpublish = promisify(publish)
+
+/* fix for "Unhandled promise rejections" */
+process.on('unhandledRejection', err => { throw err })
+
+const branch = 'gh-pages'
+/* use SSH auth by default */
+let url = 'git@github.com:status-im/ambassador.status.im.git'
+
+/* alternative is to use github used and API token */
+if (process.env.GH_USER != undefined) {
+  url = ( 
+    'https://' + process.env.GH_USER + ':' + process.env.GH_TOKEN
+    + '@github.com/status-im/analytics.status.im.git'
+  )
+}
+
+const main = async (url, branch)=> {
+  console.log(`Pushing to: ${url}`)
+  console.log(`On branch: ${branch}`)
+  await ghpublish('build', {
+    repo: url,
+    branch: branch,
+    dotfiles: true,
+    silent: false
+  })
+}
+
+main(url, branch)
